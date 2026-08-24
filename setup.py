d36c2f7008900e79ac4c3c4780111208b3d43364
pradyumna <pradyumna.kunchala@gmail.com>
2026-08-24 15:18:05 -0400

    thesis demo prep

---
+++ setup.py
@@ -0,0 +1,13 @@
+'''
+train data contains-
+1. experto ego and exo views
+2. expert pose info (this is mostly empty, will need to run third party..)
+3. learner ego and exo views
+4. learner pose info (this is mostly empty, will need to run 3rd party )
+'''
+
+DATA_ROOT = "dataset_prep/dataset_out/train"
+
+
+if __name__ == "__main__":
+    
