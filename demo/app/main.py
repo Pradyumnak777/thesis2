691d65618b1747827a444f730fa1b337e426e76e
pradyumna <pradyumna.kunchala@gmail.com>
2026-09-06 17:43:35 -0400

    building UI

---
+++ demo/app/main.py
@@ -0,0 +1,10 @@
+'''
+1. create a UI that can we viewed on teh web, from lab machine (use 127.0.0..?)
+2. Takes input of a WHAM ,pkl file or an mp4 file (this is the leaner input)
+3. runs pipeline of otkenizer+infiller, generates the pkl file for edited motion
+4. displays that beside the original
+5. show overlay (later step)
+'''
+
+
+if __name__ == "__main__":
